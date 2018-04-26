f78e39aa2bbab11c8e7bbaf6fdcd4d5eeaba5151
Jared <jbates9210@gmail.com>
2018-04-25 20:38:14 -0400

    js added to mobile menu

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,18 +1,3 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person');
-import Person from './modules/Person'; //ES6
+import MobileMenu from './modules/mobilemenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + ' has paid taxes.')
-  }
-}
-
-alert('testing 123');
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "red");
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
