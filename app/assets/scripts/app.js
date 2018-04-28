441e9f925744f8cb11c39e0ad00048b713535f50
Jared <jbates9210@gmail.com>
2018-04-27 21:46:25 -0400

    Added waypoints to reveal on scroll features section

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,5 @@
 import MobileMenu from './modules/mobilemenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
